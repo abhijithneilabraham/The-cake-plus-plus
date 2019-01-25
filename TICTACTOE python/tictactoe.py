2cca4efdef1cec8b4801e16ed8fab6bf0ee4e2bd
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-26 01:13:11 +0530

    Update TICTACTOE python/tictactoe.py

--- TICTACTOE python/tictactoe.py
+++ TICTACTOE python/tictactoe.py
@@ -9,3 +9,11 @@ Created on Sat Jan 26 01:01:08 2019
 '''
 TIC TAC TOE game
 '''
+class TicTacToe:
+    def __init__(self):
+        self.board=[[' ']*3 for j in range(3)] #creating a 3*3 board
+        self.player='X'
+        
+    def win(self):
+        
+        
